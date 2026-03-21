db7f05f7543fa57fcca58a919fcf47cfbbeaed42
jinkawazoe <jin.kawazoe@gmail.com>
2026-03-21 13:06:00 +0900

    Add GTM (GTM-PV96M93)

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,28 @@ export default function RootLayout({
       className={`h-full ${cormorant.variable} ${jost.variable}`}
       suppressHydrationWarning
     >
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PV96M93');`,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PV96M93"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
